150699ee7799070e76b074ffb7e86fef0c66a47b
jcgG42 <jactgit@gmail.com>
2017-07-03 10:54:33 +0200

    Dude, what?
    What is this nonesense?

--- Hello_World/src/hello_world/Hello_World.java
+++ Hello_World/src/hello_world/Hello_World.java
@@ -6,6 +6,7 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Pokey");
+        // Wat soll der Mist?
     }
     
 }
